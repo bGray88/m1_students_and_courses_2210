7d34c7173182e0c723a7acce6115ff2f3b4e42a8
Brandon Gray <brandonjoe88@hotmail.com>
2022-10-27 09:48:38 -0600

    adds add_course

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,17 +1,21 @@
 class Gradebook
 
-  attr_reader :instructor
-  
+  attr_reader :instructor, :courses
+
   def initialize(instructor)
     @instructor = instructor
-    @courses = []
+    @courses = Hash.new {|course, students| course[students] = []}
+  end
+
+  def add_course(course)
+    @course << course
   end
 
   def all_students
     @courses.students
   end
 
-  def below_grade
-    @courses.students.grade < theshold
+  def below_grade(threshold)
+    @courses.students.select {|student| student.grade < threshold}
   end
 end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -13,6 +13,23 @@ RSpec.describe Gradebook do
     it 'has a name, capacity, and no students upon creation' do
       gradebook = Gradebook.new("Jim")
 
-      expect(gradebook.instructor).to be_instance_of(Gradebook)
+      expect(gradebook.instructor).to eq("Jim")
     end
   end
+
+  describe '#all_students' do
+    it "can provide all students in courses collection" do
+      gradebook = Gradebook.new("Jim")
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+
+      course.enroll(student1)
+      course.enroll(student2)
+
+      gradebook.add_course(course)
+
+      expect(gradebook.courses).to eq()
+    end
+  end
+end
